afc4dadcbe3e46078193284997d0261ce431968c
JagadeeshNalluri <48944638+JagadeeshNalluri@users.noreply.github.com>
2019-11-22 09:36:07 +0530

    Add files via upload

---
+++ src/com/dineshonjava/bean/RentBean.java
@@ -0,0 +1,67 @@
+package com.dineshonjava.bean;
+
+
+public class RentBean {
+	private Integer id;
+	private String type;
+	private String bhk;
+	private String city;
+	private String state;
+	private String price;
+	private String deposite;
+	
+	
+	
+	public String getDeposite() {
+		return deposite;
+	}
+	public void setDeposite(String deposite) {
+		this.deposite = deposite;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getBhk() {
+		return bhk;
+	}
+	public void setBhk(String bhk) {
+		this.bhk = bhk;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	private String status;
+
+	
+	
+}
